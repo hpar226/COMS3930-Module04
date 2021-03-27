a332f90d3f884d29dd4335bc919b0e60233813f3
hpar226 <55038398+hpar226@users.noreply.github.com>
2021-03-26 21:26:02 -0400

    Add files via upload

---
+++ sketch_cat_toy-v1.ino
@@ -0,0 +1,127 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#define USE_SERIAL Serial
+#include <ESP32Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int posVal = 0;    // variable to store the servo position
+int servoPin = 15; // Servo motor pin
+
+// Conncet the port of the stepper motor driver
+int outPorts[] = {33, 27, 26, 25};
+
+
+const char *ssid_Router     = "xxxxx"; //Enter the router name
+const char *password_Router = "xxxxx"; //Enter the router password
+
+String address= "http://165.227.76.232:3000/hp2380/running";
+
+void setup(){
+  
+  USE_SERIAL.begin(115200);
+
+  WiFi.begin(ssid_Router, password_Router);
+  USE_SERIAL.println(String("Connecting to ")+ssid_Router);
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    USE_SERIAL.print(".");
+  }
+  USE_SERIAL.println("\nConnected, IP address: ");
+  USE_SERIAL.println(WiFi.localIP());
+  USE_SERIAL.println("Setup End");
+    
+  // set pins to output
+  for (int i = 0; i < 4; i++) {
+    pinMode(outPorts[i], OUTPUT);
+  }
+  
+  myservo.setPeriodHertz(50);           // standard 50 hz servo
+  myservo.attach(servoPin, 500, 2500);  // attaches the servo on servoPin to the servo object
+
+}
+ 
+void loop() {
+  
+  if((WiFi.status() == WL_CONNECTED)) {
+    HTTPClient http;
+    http.begin(address);
+ 
+    int httpCode = http.GET(); // start connection and send HTTP header
+    if (httpCode == HTTP_CODE_OK) { 
+        String response = http.getString();
+        if (response.equals("false")) {
+            // Do not run sculpture, perhaps sleep for a couple seconds
+            delay(2000);
+        }
+        else if(response.equals("true")) {
+            // Run sculpture
+            loopStepper();
+        }
+        USE_SERIAL.println("Response was: " + response);
+    } else {
+        USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();
+    delay(500); // sleep for half of a second
+  }
+}
+
+
+void loopStepper()
+{
+  loopServo();
+  // Rotate a full turn
+  moveSteps(true, 32 * 64, 3);
+  delay(1000);
+  loopServo();
+  // Rotate a full turn towards another direction
+  moveSteps(false, 32 * 64, 3);
+  delay(1000);
+}
+
+void loopServo()
+{
+  for (posVal = 0; posVal <= 180; posVal += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(posVal);       // tell servo to go to position in variable 'pos'
+    delay(5);                   // waits 15ms for the servo to reach the position
+  }
+ 
+  for (posVal = 180; posVal >= 0; posVal -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(posVal);       // tell servo to go to position in variable 'pos'
+    delay(5);                   // waits 15ms for the servo to reach the position
+  }
+}
+
+//Suggestion: the motor turns precisely when the ms range is between 3 and 20
+void moveSteps(bool dir, int steps, byte ms) {
+  for (unsigned long i = 0; i < steps; i++) {
+    moveOneStep(dir); // Rotate a step
+    delay(constrain(ms,3,20));        // Control the speed
+  }
+}
+
+void moveOneStep(bool dir) {
+  // Define a variable, use four low bit to indicate the state of port
+  static byte out = 0x01;
+  // Decide the shift direction according to the rotation direction
+  if (dir) {  // ring shift left
+    out != 0x08 ? out = out << 1 : out = 0x01;
+  }
+  else {      // ring shift right
+    out != 0x01 ? out = out >> 1 : out = 0x08;
+  }
+  // Output singal to each port
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(outPorts[i], (out & (0x01 << i)) ? HIGH : LOW);
+  }
+}
+
+void moveAround(bool dir, int turns, byte ms){
+  for(int i=0;i<turns;i++)
+    moveSteps(dir,32*64,ms);
+}
+void moveAngle(bool dir, int angle, byte ms){
+  moveSteps(dir,(angle*32*64/360),ms);
+}
